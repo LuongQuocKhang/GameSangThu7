5d118eb1db7cb888c58ffa8d0c9628b6b26d9eb4
LuongQuocKhang <KhangHuu97@gmail.com>
2019-04-17 17:03:47 +0700

    Revert "Update ninja"
    This reverts commit e5d3007867f9901750cf24d81866bfd7e06d9065.

---
+++ 03-Keyboard-States/Camera.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <DirectXMath.h>;
+#include "Sprites.h";
+#include "Ninja.h"
+class CGame;
+
+class Camera
+{
+public:
+	Camera(int width, int height, float angle , DirectX::XMFLOAT3 scaleFactors);
+	~Camera();
+
+	void Update();
+	void Follow(Ninja * following);
+	bool IsFollowing() const;
+
+	void SetTransform(CGame *Device) const;
+
+private:
+	float angle;
+	DirectX::XMFLOAT3 scaleFactors;
+	D3DXMATRIX orthographicMatrix;
+	D3DXMATRIX identityMatrix;
+	D3DXMATRIX ViewMatrix;
+
+	Ninja *following;
+
+	int width;
+	int height;
+};
+
+
+#endif // !CAMERA_H
+
+
---
+++ 03-Keyboard-States/Ninja.cpp
@@ -0,0 +1,109 @@
+#include "Ninja.h"
+
+
+
+Ninja::Ninja()
+{
+}
+
+
+Ninja::~Ninja()
+{
+}
+
+void Ninja::Update(DWORD dt)
+{
+	CGameObject::Update(dt);
+
+	// simple fall down
+	vy += NINJA_GRAVITY;
+	if (y > 100)
+	{
+		vy = 0; y = 100.0f;
+	}
+
+	// simple screen edge collision!!!
+	/*if (vx > 0 && x > 290) x = 290;
+	if (vx < 0 && x < 0) x = 0;*/
+	if (jp == true)
+	{
+		countTime += dt;
+		if (countTime >= dt * 8 * 2)
+		{
+			countTime = 0;
+			jp = false;
+		}
+	}
+}
+
+void Ninja::Render()
+{
+	int ani;
+	if (vx == 0)
+	{
+		if (nx > 0) ani = NINJA_ANI_IDLE_RIGHT;
+		else ani = NINJA_ANI_IDLE_LEFT;
+		if (sd == true)
+		{
+			if (nx > 0) ani = NINJA_ANI_SIT_RIGHT;
+			else ani = NINJA_ANI_SIT_LEFT;
+		}
+		if (jp == true)
+		{
+			if (nx > 0) ani = NINJA_ANI_JUMP_RUN_RIGHT;
+			else ani = NINJA_ANI_JUMP_RUN_LEFT;
+		}
+	}
+	else if (vx > 0)
+	{
+		ani = NINJA_ANI_WALKING_RIGHT;
+		if (jp == true)
+		{
+			if (nx > 0) ani = NINJA_ANI_JUMP_RUN_RIGHT;
+		}
+	}
+	else
+	{
+		ani = NINJA_ANI_WALKING_LEFT;
+		if (jp == true)
+		{
+			if (nx <= 0) ani = NINJA_ANI_JUMP_RUN_LEFT;
+		}
+	}
+
+	//animations[ani]->Render(x, y);
+}
+
+void Ninja::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case NINJA_STATE_WALKING_RIGHT:
+		vx = NINJA_WALKING_SPEED;
+		nx = 1;
+		break;
+	case NINJA_STATE_WALKING_LEFT:
+		vx = -NINJA_WALKING_SPEED;
+		nx = -1;
+		break;
+	case NINJA_STATE_JUMP:
+		if (y == 100)
+			vy = -NINJA_JUMP_SPEED_Y;
+		break;
+	case NINJA_STATE_IDLE:
+		vx = 0;
+		break;
+	case NINJA_STATE_JUMP_RIGHT:
+		if (y == 100)
+			vy = -NINJA_JUMP_SPEED_Y;
+		vx = NINJA_WALKING_SPEED;
+		break;
+	case NINJA_STATE_JUMP_LEFT:
+		if (y == 100)
+			vy = -NINJA_JUMP_SPEED_Y;
+		vx = -NINJA_WALKING_SPEED;
+		break;
+	}
+}
+
