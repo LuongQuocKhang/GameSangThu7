8e4e160407a928437996ead5c213f50a40363717
LuongQuocKhang <KhangHuu97@gmail.com>
2019-04-17 17:03:43 +0700

    Revert "commit revert"
    This reverts commit fc08a3091bfc46925bf9f8e212cf70b2505ee7ab.

---
+++ 03-Keyboard-States/Animation.cpp
@@ -0,0 +1,36 @@
+﻿#include "Animation.h"
+
+void Animation::AddFrame(Sprite * sprite, DWORD time)
+{
+	DWORD t = time;
+	pair <Sprite *, DWORD> frame(sprite, t);
+	frames.push_back(frame);
+}
+void Animation::Render(SpriteData spriteData)
+{
+	DWORD now = GetTickCount();
+	if (curFrame == -1)
+	{
+		curFrame = 0;
+		lastFrameTime = now;
+		done = false;
+	}
+
+	frames[curFrame].first->SetData(spriteData);
+	CGame::GetInstance()->Draw(frames[curFrame].first);
+
+	DWORD t = frames[curFrame].second;
+	
+	if (now - lastFrameTime > t)
+	{
+		curFrame++;
+		lastFrameTime = now;
+		if (curFrame == frames.size())
+		{
+			curFrame = 0;
+			done = true;
+		}
+		else
+			done = false;
+	}
+}
